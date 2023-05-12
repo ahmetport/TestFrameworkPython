aff41165c748e9e0fe61e47ce2542c177541d0ff
ahmetport <umitenes89@gmail.com>
2023-05-12 16:34:38 +0300

    first

--- pages/arama_sayfası.py
+++ pages/arama_sayfası.py
@@ -1,27 +1,5 @@
-import selenium
-from selenium.webdriver import Keys
-from selenium.webdriver.common.by import By
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from pages.PageBase import PageBase
 
-class AramaSayfasi(PageBase):
 
-    def __init__(self,driver):
-        super().__init__(driver)
-        self.driver=driver
-
-    ARAMA_KUTUSU=(By.XPATH,"//input[@id='small-searchterms']")
-    ARAMA_MESAJI=(By.CSS_SELECTOR,"div.search-results")
-
-    def arama_yap(self,kelime):
-        arama_kutusu=self.driver.find_element(*AramaSayfasi.ARAMA_KUTUSU)
-        arama_kutusu.send_keys(kelime)
-        arama_kutusu.send_keys(Keys.ENTER)
-
-    def mesaj_ver(self):
-        mesaj=self.driver.find_element(*AramaSayfasi.ARAMA_MESAJI).text.strip()
-        return mesaj
 
 
 
--- tests/test_arama.py
+++ tests/test_arama.py
@@ -1,25 +1,5 @@
-import pytest
-import time
-import unittest
-
-from pages.arama_sayfası import AramaSayfasi
-from ddt import ddt, data, file_data, idata, unpack
 
 
 
-
-
-@pytest.mark.usefixtures("setup")
-@ddt
-class TestArama(unittest.TestCase):
-    @data(*excelYardımcı.excel_listeler_listesine_cevir("./testdata/arama.xls","Personel Muafiyet Listesi"))
-    @unpack
-
-    def test_ikiharfli_üçharfli_aramayap(self,kelime,beklenen_mesaj):
-        arama=AramaSayfasi(self.driver)
-        arama.arama_yap(kelime)
-        mesaj=arama.mesaj_ver()
-        time.sleep(3)
-        assert mesaj == beklenen_mesaj
 
 
